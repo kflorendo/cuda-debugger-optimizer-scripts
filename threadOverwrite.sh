917562d3611b5b7c6a77cf124c450323b9c90ed4
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-03 17:59:26 -0400

    Update threadOverwrite.sh

--- threadOverwrite.sh
+++ threadOverwrite.sh
@@ -80,7 +80,7 @@ declare -A written
 while IFS= read -r line; do
     data="$line"
     stringarray=($data)
-    written[${stringarray[6]}]="${written[${stringarray[6]}]}${written[${stringarray[6]}]:+,}((${stringarray[8]}), ${stringarray[0]}, ${stringarray[1]}, ${stringarray[2]}, ${stringarray[3]}, ${stringarray[4]}, ${stringarray[5]}, [${stringarray[7]}])"
+    written[${stringarray[6]}]="${written[${stringarray[6]}]}${written[${stringarray[6]}]:+,}${stringarray[0]},${stringarray[1]},${stringarray[2]},${stringarray[3]},${stringarray[4]},${stringarray[5]},${stringarray[8]},${stringarray[7]}"
 done < out.txt
 
 #source: https://stackoverflow.com/questions/27832452/associate-multiple-values-for-one-key-in-array-in-bash
@@ -93,7 +93,7 @@ do
     count=$(((count + 1) / 2))
     if [ $count -gt 1 ]
     then
-        echo "address ${key} written to by ${written[$key]}" >> threadOverwrite.txt
+        echo "${key} ${written[$key]}" >> threadOverwrite.txt
     fi
 done
 
