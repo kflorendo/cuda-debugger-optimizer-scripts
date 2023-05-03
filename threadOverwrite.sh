b6df5319d29d36ee5f485c1b8ac86cbd61bd4118
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-03 10:54:49 -0400

    updated threadOverwrite

--- threadOverwrite.sh
+++ threadOverwrite.sh
@@ -7,7 +7,7 @@
 # -c = path to .cu file
 # ./threadOutput.sh -m "/afs/andrew.cmu.edu/usr16/kflorend/private/15418/project/15418-asst2/scan" -r "/afs/andrew.cmu.edu/usr16/kflorend/private/15418/project/15418-asst2/scan/cudaScan -m scan -i random -n 100" -c "/afs/andrew.cmu.edu/usr16/kflorend/private/15418/project/15418-asst2/scan/scan.cu"
 
-while getopts m:r:c:v:l: flag
+while getopts m:r:c:v:l:t:a: flag
 do
     case "${flag}" in
         m) makeDir=${OPTARG};;
@@ -15,6 +15,8 @@ do
         c) cuPath=${OPTARG};;
         v) varName=${OPTARG};;
         l) lineNum=${OPTARG};;
+        t) varType=${OPTARG};;
+        a) arrayOpt=${OPTARG};;
     esac
 done
 echo "Make dir: $makeDir";
@@ -22,6 +24,8 @@ echo "Run command: $runCmd";
 echo "Path to .cu file: $cuPath"; 
 echo "Variable tracked: $varName";
 echo "Insert at line: $lineNum";
+echo "Variable type: $varType";
+echo "Is array: $arrayOpt";
 
 # save current contents to temp file
 tempCu="temp-$RANDOM.cu"
@@ -34,7 +38,19 @@ prefix="cuda-debug-optimize$(date +%s)"
 insertLine=1
 # add print statements into file
 # sed -i "${insertLine} i printf(\"blockIdx threadIdx address\");" $cuPath
-sed -i "${lineNum} i printf(\"${prefix} %d %d %d %d %d %d ${varFormat}\\\\n\", blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z, &${varName});" $cuPath
+case $varType in
+    "int") varFormat="%d";;
+    "float") varFormat="%f";;
+    "string") varFormat="%s";;
+esac
+
+case $arrayOpt in
+    "y") tmp = ${varName#*'['} #remove everything before [
+        index = ${tmp%']'*};;   #remove everything after ]
+    "n") :
+esac
+
+sed -i "${lineNum} i printf(\"${prefix} %d %d %d %d %d %d %u ${varFormat}\\\\n\", blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z, &${varName}, ${varName}, ${index});" $cuPath
 # address column needed when var input is array (include index, multiple addresses)
 
 
@@ -52,7 +68,7 @@ declare -A written
 while IFS= read -r line; do
     data="$line"
     stringarray=($data)
-    written[${stringarray[2]}]="${written[${stringarray[2]}]}${written[${stringarray[2]}]:+,}(${stringarray[0]}, ${stringarray[1]})"
+    written[${stringarray[6]}]="${written[${stringarray[6]}]}${written[${stringarray[6]}]:+,}(${stringarray[0]}, ${stringarray[1]}, ${stringarray[2]}, ${stringarray[3]}, ${stringarray[4]}, ${stringarray[5]}, ${stringarray[7]})"
 done < out.txt
 
 #source: https://stackoverflow.com/questions/27832452/associate-multiple-values-for-one-key-in-array-in-bash
