dafb8cf741176ec06c0999fc525a49b4b91d9ab7
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-03 19:50:35 -0400

    Update gprofTime.py

--- gprofTime.py
+++ gprofTime.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 
 time = []
 name = []
+linenum = 0;
 
 with open('results.txt', 'r') as f:
-    for line in f:
-        if not line.startswith(' '):
-            continue
-
-        time.append(line[7:14].strip())
-        name.append(line[47:].strip())
-
+    if (linenum > 4):
+        for line in f:
+            if not line.startswith(' '):
+                continue
+            cleanline = line.strip()
+            arr = cleanline.split("\\s")
+            time.append(arr[0])
+            name.append(arr[6])
+    linenum += 1
 
 plt.pie(time, labels=name, autopct='%1.1f%%')
 
