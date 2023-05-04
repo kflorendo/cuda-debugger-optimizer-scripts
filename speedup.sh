fd04bdfac87f63d30a1c444128a843f2ee54f70e
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-04 00:52:31 -0400

    Add files via upload

---
+++ speedup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while getopts s:p: flag
+do
+    case "${flag}" in
+        s) seqRun=${OPTARG};;
+        p) paraRun=${OPTARG};;
+    esac
+done
+echo "Sequential File Path: $seqRun";
+echo "Parallel File Path: $paraRun";
+
+start_time=$(date +%s.%6N)
+$seqRun
+end_time=$(date +%s.%6N)
+elapsed=$(echo "scale=6; $end_time - $start_time" | bc)
+
+start_time2=$(date +%s.%6N)
+$paraRun
+end_time2=$(date +%s.%6N)
+elapsed2=$(echo "scale=6; $end_time2 - $start_time2" | bc)
+
+bc -l <<< '${elapsed}/${elapsed2}'
