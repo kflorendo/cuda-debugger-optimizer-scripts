1020e1ebe082d25b076584fb13b0fce046080080
kflorendo <kflorendo1012@gmail.com>
2023-05-04 02:18:30 -0400

    Update memoryBottleneck.sh

--- memoryBottleneck.sh
+++ memoryBottleneck.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+while getopts r: flag
+do
+    case "${flag}" in
+        r) runCmd=${OPTARG};;
+    esac
+done
+
+nvprof --print-gpu-trace --csv --log-file output/timeBottleneckGpuTrace.txt --normalized-time-unit us $runCmd
+
+python3 memoryBottleneck.py
