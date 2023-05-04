4f0117ec1366b4c5818c14f782432aae7e59febb
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-04 03:18:54 -0400

    Add files via upload

--- memoryBottleneck.py
+++ memoryBottleneck.py
@@ -54,3 +54,93 @@ plt.xlabel('Time(s)')
 leg = plt.legend(loc='lower center')
 plt.savefig("timestat", dpi='figure', format=None)
 plt.clf()
+
+
+timedynamX = {}
+timedynamY = {}
+for i in range(len(name)):
+    if (dynamicMem[i] != ''):
+        if name[i] in timedynamX:
+            timedynamX[name[i]].append(float(time[i]))
+        else:
+            timedynamX[name[i]] = [float(time[i])]
+        
+        if name[i] in timedynamY:
+            timedynamY[name[i]].append(float(dynamicMem[i]))
+        else:
+            timedynamY[name[i]] = [float(dynamicMem[i])]
+
+default_cycler = (cycler(color=['r', 'g', 'b', 'y', 'm', 'c']))
+
+
+plt.rc('axes', prop_cycle=default_cycler)
+
+for each in timedynamX:
+    plt.plot(timedynamX[each], timedynamY[each], label = each)
+
+plt.title('dynamicMem vs Time')
+plt.ylabel('dynamicMem')
+plt.xlabel('Time(s)')
+leg = plt.legend(loc='lower center')
+plt.savefig("timedynam", dpi='figure', format=None)
+plt.clf()
+
+
+timesizeX = {}
+timesizeY = {}
+for i in range(len(name)):
+    if (size[i] != ''):
+        if name[i] in timesizeX:
+            timesizeX[name[i]].append(float(time[i]))
+        else:
+            timesizeX[name[i]] = [float(time[i])]
+        
+        if name[i] in timesizeY:
+            timesizeY[name[i]].append(float(size[i]))
+        else:
+            timesizeY[name[i]] = [float(size[i])]
+
+default_cycler = (cycler(color=['r', 'g', 'b', 'y', 'm', 'c']))
+
+
+plt.rc('axes', prop_cycle=default_cycler)
+
+for each in timesizeX:
+    plt.plot(timesizeX[each], timesizeY[each], label = each)
+
+plt.title('size vs Time')
+plt.ylabel('size')
+plt.xlabel('Time(s)')
+leg = plt.legend(loc='lower center')
+plt.savefig("timesize", dpi='figure', format=None)
+plt.clf()
+
+
+timethruX = {}
+timethruY = {}
+for i in range(len(name)):
+    if (throughput[i] != ''):
+        if name[i] in timethruX:
+            timethruX[name[i]].append(float(time[i]))
+        else:
+            timethruX[name[i]] = [float(time[i])]
+        
+        if name[i] in timethruY:
+            timethruY[name[i]].append(float(throughput[i]))
+        else:
+            timethruY[name[i]] = [float(throughput[i])]
+
+default_cycler = (cycler(color=['r', 'g', 'b', 'y', 'm', 'c']))
+
+
+plt.rc('axes', prop_cycle=default_cycler)
+
+for each in timethruX:
+    plt.plot(timethruX[each], timethruY[each], label = each)
+
+plt.title('throughput vs Time')
+plt.ylabel('throughput')
+plt.xlabel('Time(s)')
+leg = plt.legend(loc='lower center')
+plt.savefig("timethru", dpi='figure', format=None)
+plt.clf()
