c2739730a5248c7b0905e587dc3458447f40d155
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-04 04:44:58 -0400

    Update sortThreads.py

--- sortThreads.py
+++ sortThreads.py
@@ -1,14 +1,14 @@
 
 threads = []
 
-for line in open('threadbp.txt', 'r'):
+for line in open('output/threadbp.txt', 'r'):
     threads.append(line)
 
 ordered = sorted(threads)
 
 # clear file
-with open("threadbp.txt", "w") as file:
+with open("output/threadbp.txt", "w") as file:
     pass
 
-with open("threadbp.txt", "w") as file:
+with open("output/threadbp.txt", "w") as file:
     file.writelines("%s\n" % thread for thread in ordered)
