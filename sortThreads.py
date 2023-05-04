eb117329a07729a847aa9851c40cbdbe0663d75b
huiningliang <60528661+huiningliang@users.noreply.github.com>
2023-05-04 01:42:22 -0400

    write to same file

--- sortThreads.py
+++ sortThreads.py
@@ -5,3 +5,10 @@ for line in open('threadbp.txt', 'r'):
     threads.append(line)
 
 ordered = sorted(threads)
+
+# clear file
+with open("threadbp.txt", "w") as file:
+    pass
+
+with open("threadbp.txt", "w") as file:
+    file.writelines("%s\n" % thread for thread in ordered)
